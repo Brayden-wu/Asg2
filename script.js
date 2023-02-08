6b51d3484b4b2ba45abe26c9b5f45117f273c215
Brayden-wu <s10221128@connect.np.edu.sg>
2023-02-09 01:15:01 +0800

    implementing pop up for magazine page, created page2 for magazine to try things out on that web page as draft

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-document.getElementById("button").addEventListener("click", function(){
-    document.querySelector(".popup").getElementsByClassName.display = "flex";
-})
+const books = document.querySelectorAll('.col-4');
+for (const book of books) {
+  const image = book.querySelector('img');
+  image.addEventListener('click', function() {
+    const title = book.querySelector('h4').textContent;
+    const description = 'Description for book: ' + title;
 
-document.getElementById("close").addEventListener("click", function(){
-    document.querySelector(".popup").getElementsByClassName.display = "none";
-})
+    alert(description);
+  });
+}
