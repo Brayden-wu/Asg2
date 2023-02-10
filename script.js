aad05c3e3c0ec448b84c39720a69eb7a1efc3628
Brayden-wu <s10221128@connect.np.edu.sg>
2023-02-10 18:15:16 +0800

    trying to pull from github

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
-const books = document.querySelectorAll('.col-4');
-for (const book of books) {
-  const image = book.querySelector('img');
-  image.addEventListener('click', function() {
-    const title = book.querySelector('h4').textContent;
-    const description = 'Description for book: ' + title;
+function showBookDetails(title, imageUrl, price) {
+  const bookDetails = `
+  <div class="book-details">
+      <img src="${imageUrl}" alt="${title}" width="200px" height="200px">
+      <h4>${title}</h4>
+      <p1>Price: $${price}</p1>
+      <button id="add-to-cart-button">Add to Cart</button>
+  </div>
+  `;
 
-    alert(description);
+  const overlay = document.createElement("div");
+  overlay.innerHTML = bookDetails;
+  overlay.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+  overlay.style.position = "fixed";
+  overlay.style.top = "0";
+  overlay.style.bottom = "0";
+  overlay.style.left = "0";
+  overlay.style.right = "0";
+  overlay.style.display = "flex";
+  overlay.style.alignItems = "center";
+  overlay.style.justifyContent = "center";
+  document.body.appendChild(overlay);
+
+  overlay.addEventListener("click", function(event) {
+      if (event.target.id === "add-to-cart-button") {
+          addToCart(title, price);
+      } else {
+          document.body.removeChild(overlay);
+      }
   });
 }
+
+const books = document.getElementsByClassName("col-4");
+for (const book of books) {
+  const img = book.getElementsByTagName("img")[0];
+  const title = book.getElementsByTagName("h4")[0].innerHTML;
+  const price = book.getElementsByTagName("p1")[0].innerHTML.split(" ")[1];
+  img.addEventListener("click", function() {
+      showBookDetails(title, img.src, price);
+  });
+}
+
